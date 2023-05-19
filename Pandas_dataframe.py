8e42b033ccd097d1a66ca533f61bcd8028ed7ab5
Parth Dande <parthdande15@gmail.com>
2023-05-19 15:07:00 +0530

    pandas series updated
    updated the file name pandas and added some more function used in the pandas series function

---
+++ Pandas_dataframe.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 15:04:44 2023
+
+@author: parth
+"""
+
--- pandas.py
+++ pandas.py
@@ -25,3 +25,20 @@ f = np.array(['a','b','c','d','e'])
 data = {'a':0,'b':1,'c':2}
 s = pd.Series(data)
 print(s)
+
+###############
+'''
+data={'a':0,'b':1,'c':2}
+s = pd.Series(data,index = ['b','c','d','e'])
+print(s)'''
+
+#Index order is persisted and the msising element 
+#is filled with NaN (Not a Number)
+
+##########
+'''
+s = pd.Series(5,index = [0,1,2,3,4])
+print(s)
+'''
+#If data is a scalar value , an index must be provided
+#the value will be repeated to match the length of index
